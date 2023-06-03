a978d011cf4fb77f4e92a2b824a968770556cb1d
Thinh Pham <thinhph9@gmail.com>
2023-06-03 09:03:34 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,12 +23,10 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu>
-
-        </Menu>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
-  
+
 }
 export default App;
