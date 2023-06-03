8792c35e0d443a45fb3e570c6ee5858871954fe6
Thinh Pham <thinhph9@gmail.com>
2023-06-03 11:02:47 +0700

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import React, { Component } from 'react';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
-
-
+import Header from './components/HeaderComponent';
+import Footer from './components/FooterComponent';
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Header />
         <Main />
+        <Footer/>
       </div>
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,8 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 //import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
